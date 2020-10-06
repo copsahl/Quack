6a2cdb103afaa57ac3a097f513e9d4da3c630c16
Chase Opsahl <cjopsahl99@gmail.com>
2020-10-06 09:14:04 -0500

    Updated main.c to display Quack Usage

--- main.c
+++ main.c
@@ -7,26 +7,37 @@ int main(void){
 	Quack_t *q;
 	int i;
 
+	// Creating the Quack!
 	q = buildQuack();
+
+	// Adding 100 pseudo-random numbers to the Quack
+	printf("Filling Quack!\n\n");
 	for(i = 0; i < 100; i++){
 		addQuackNode(q, i * 45 % 6 +12 / 5 * 4 + i);
 	}
+	
+	// Searching the Quack
+	printf("\n\nSearching Quack!\n\n");
 	if(searchVal(q, 12) == 1){
 		printf("Value Found!\n");
 	}
 
-	if(searchVal(q, 14) == 0){
+	if(searchVal(q, 100000) == 0){
 		printf("Value Not Found!\n");
 	}
 
+	// Printing the Quack
+	printf("\n\nPrinting the Quack!\n\n");
 	printQuack(q);
-	printf("3rd to last: %d\n", q->end->prev->prev->val);
-	printf("2nd to last: %d\n", q->end->prev->val);
-	printf("Last: %d\n", q->end->val);
+
+	// Popping from the Quack
+	printf("\n\nPopping from the Quack!\n\n");
 	for(i = 0; i < 15; i++){
 		printf("%d\n", duckduckGrayduck(q, 0));
 	}
 	
+	// Printing the new Quack
+	printf("\n\nPrinting Quack!\n\n");
 	printQuack(q);
 
 	return 0;
